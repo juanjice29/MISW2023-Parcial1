b73a747332dbe90ed495f95d9bd9d072cf625d36
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2023-04-27 22:52:37 -0500

    Creacion modulo y componente de plantas

---
+++ src/app/plantas/plantas-list/plantas-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plantas-list',
+  templateUrl: './plantas-list.component.html',
+  styleUrls: ['./plantas-list.component.css']
+})
+export class PlantasListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
